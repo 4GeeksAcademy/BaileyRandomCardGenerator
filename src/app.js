7a62de5a471f00471efeb9b3fffa8b8333d18efc
Kathy Bailey Hines <135627670+klbailey@users.noreply.github.com>
2023-12-01 01:06:57 +0000

    Added button

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+// import "./index.html";
 
 window.onload = function() {
   document.querySelector(".card").classList.add(generateRandomSuit());
   document.querySelector(".card").innerHTML = generateRandomNum();
 };
 
-// let button = document.querySelector("#button");
-// button.addEventListener("click", () => {
-//   document.querySelector(".card").classList.add(generateRandomSuit());
-//   document.querySelector(".card").innerHTML = generateRandomNum();
-// });
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let card = document.querySelector(".card");
+  let removeClassName = card.classList["1"];
+  //remove suit/class from onload
+  document.querySelector(".card").classList.remove(removeClassName);
+  //add new random suit/class
+  card.classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNum();
+});
 let generateRandomNum = () => {
   let numbers = [
     "A",
